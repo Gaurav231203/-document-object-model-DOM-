c02b4c018dcc7f6de9b082b88a35d496898e2034
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-05 00:43:39 +0530

    Added new files

---
+++ textmanupulation.js
@@ -0,0 +1,7 @@
+let h1=document.querySelector("h1");
+h1.innerText="mtt le itni tension"
+h1.textContent="le hee qu rha hai"
+console.dir(h1);
+h1.innerHTML="<i>hello</i>"
+//innerHTML text  nshi badalta ye html badal ta hai
+//is ka matlat innerHTML kisi bhi selsct kiye hue elemnet ka html badal deta hai
