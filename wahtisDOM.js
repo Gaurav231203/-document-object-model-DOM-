809b67d050ff272e3d1350a1787887f0b8680e09
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-04 15:44:02 +0530

    Added new files

---
+++ wahtisDOM.js
@@ -0,0 +1,45 @@
+// dom - is a document object model
+//DOM browser dwara HTML ka banaya hua JavaScript object hai.
+/*Why? (Sabse important)
+
+Socho tumhare paas HTML hai:
+
+<h1>Hello Gaurav</h1>
+
+HTML sirf text file hai.
+
+JavaScript us text file ko directly samajh nahi sakti.
+
+To browser kya karta hai?
+
+Step 1
+
+HTML padhta hai.
+
+<html>
+    <body>
+        <h1>Hello Gaurav</h1>
+    </body>
+</html>
+
+↓
+
+Step 2
+
+Browser isko objects mein convert karta hai.
+
+Document
+ │
+ ▼
+html
+ │
+ ▼
+body
+ │
+ ▼
+h1
+ │
+ ▼
+Hello Gaurav
+
+Ye poora tree hi DOM hai.*/
