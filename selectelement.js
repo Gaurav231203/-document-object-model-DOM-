1d3ffaf0dd1f067654e195eb20b501a4c5f9a45d
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-04 23:18:31 +0530

    Added new files

---
+++ selectelement.js
@@ -0,0 +1,11 @@
+/*document.getElementById
+document.getElementsByClassName
+document.getElementsByName
+document.getElementsByTagName
+document.getElementsByTagNameNS
+document.querySelector
+document.querySelectorAll*/
+let abcd=document.getElementById("abcd");
+let beta= document.getElementsByClassName("hii");
+console.log(beta);
+console.dir(abcd);
