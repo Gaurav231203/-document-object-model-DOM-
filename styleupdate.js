2876aac171f0bf084a6a8641798ed5ce78c49538
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-06 02:04:44 +0530

    Added new files

---
+++ styleupdate.js
@@ -0,0 +1,11 @@
+//js say css badalna sekhoooo 
+let h1= document.querySelector("h1");
+h1.style.color="blue";
+h1.style.fontFamily="Courier New";
+h1.style.textTransform="capitalize"
+h1.classList.add("same-same");
+let h2=document.querySelector("h2")
+h2.classList.remove("blue");
+//console.dir(h1)
+and toggel ye kta hai ki agr class lagi hai to hata dega 
+or class hati hai laga dega
