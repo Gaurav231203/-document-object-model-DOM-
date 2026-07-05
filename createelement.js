463434caec1bfed24578b44d355446e094481b1b
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-06 00:28:09 +0530

    Added new files

---
+++ createelement.js
@@ -0,0 +1,8 @@
+//1. element create kro
+// element ko append ya prepend kro mtlb aage ya peeche kro jaha bhi chye 
+// .
+let h2=document.createElement("h2");
+//console.dir(h2);
+h2.textContent="dhyan de";
+//document.querySelector("body").prepend(h2);
+document.body.prepend(h2)
--- removeattribute.js
+++ removeattribute.js
@@ -1,2 +1,4 @@
 let a=document.querySelector("a")
 console.dir(a.removeAttribute("href"));
+let img = document.querySelector("img");
+console.dir(img.setAttribute( "src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1V-eZ_ggCX1_0wJY3l4biv3ZVYl7M_aNuZ7xrPAplfg&s=10"))
