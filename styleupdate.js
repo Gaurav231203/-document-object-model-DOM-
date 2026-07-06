f6fc019ecb772fc9a3e3c67e5b699bad09ab0675
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-06 15:34:55 +0530

    Added new files

--- styleupdate.js
+++ styleupdate.js
@@ -1,11 +1,11 @@
 //js say css badalna sekhoooo 
 let h1= document.querySelector("h1");
-h1.style.color="blue";
-h1.style.fontFamily="Courier New";
-h1.style.textTransform="capitalize"
-h1.classList.add("same-same");
+//h1.style.color="blue";
+//h1.style.fontFamily="Courier New";
+//h1.style.textTransform="capitalize"
+h1.classList.add("red");
 let h2=document.querySelector("h2")
 h2.classList.remove("blue");
 //console.dir(h1)
-and toggel ye kta hai ki agr class lagi hai to hata dega 
-or class hati hai laga dega
+/*and toggel ye kta hai ki agr class lagi hai to hata dega 
+or class hati hai laga dega*/
