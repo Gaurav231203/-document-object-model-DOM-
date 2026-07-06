efb8b39d33a082885e8fe7680a02cb102acd8332
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-06 17:46:03 +0530

    Added new files

---
+++ headding.js
@@ -0,0 +1,10 @@
+//select A HEADDING FROM A APAGE BYE ITS ID AND 
+// CANGE  ITS CONTENT OR TEXT TO PADHOO BHAI;
+
+/*let changed =document.querySelector("#head");
+changed.textContent="padh dho bhai";*/
+ let h1= document.querySelector("#head")
+ h1.textContent="padho bhai " 
+ h1.style.color="blue"
+ // one mire waay to do it 
+ //  document.querrySelector("#head").innerHTML="padho bhai"
