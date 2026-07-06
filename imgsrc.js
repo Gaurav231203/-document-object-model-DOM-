da3df886df386d6bba1a1973b4b143c8c6672aa5
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-07 00:30:47 +0530

    Added new files

---
+++ imgsrc.js
@@ -0,0 +1,16 @@
+//how willl i get image src using js
+{
+/*let img=document.querySelector("img");
+console.log(img.getAttribute("src"));*/
+}
+//one meore way;
+{
+//console.log(img.src);
+}
+
+// what dose setAttribute() do?
+document.querySelector("img").setAttribute("src" , "https://images.unsplash.com/photo-1782771116101-0320275aa1b2?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+{
+//we just set the new attribute to the allready given attribute
+}
