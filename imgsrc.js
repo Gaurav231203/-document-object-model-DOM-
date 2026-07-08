093aab5d439e17245324298550d6c83e13fc6ecf
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-08 23:42:56 +0530

    Added new files

--- imgsrc.js
+++ imgsrc.js
@@ -14,3 +14,12 @@ document.querySelector("img").setAttribute("src" , "https://images.unsplash.com/
 {
 //we just set the new attribute to the allready given attribute
 }
+{
+// create a new image element with a placeholder source and add it at the top of the div
+
+}
+let image= document.createElement("img");
+image.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjCdPTR5mXVlGkSvBU69wpgWyUDft8jvAE3LCsbaFO-9zHxPWbYnuezfFG&s=10")
+
+
+let div= document.querySelector("div").prepend(image);
