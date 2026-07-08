47a24146e7f6d00ba7e720ddb96caf0e3e7c4da3
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-08 16:48:57 +0530

    Added new files

--- ulliforloop.js
+++ ulliforloop.js
@@ -1,14 +1,21 @@
 // select all li elementsand print there text using a loops
 
-let list= document.querySelectorAll("li");
+//let list= document.querySelectorAll("li");
 /* list.forEach(function(val){
    console.log(val.textContent);
 
  });*/ 
  //ONE MORE WAY
- for(i=0;i<list.length;i++){
-    console.log(list[i].textContent);
- }
+ //for(i=0;i<list.length;i++){
+//    console.log(list[i].textContent);
+// }
+ //moving thi li to the last of the ul 
     
- 
+ let hi=document.querySelector("ul"); 
+  let li=document.createElement("li")
+  li.textContent="hllo"
+
+ hi.appendChild(li);
+
+
 
