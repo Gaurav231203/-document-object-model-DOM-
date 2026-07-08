700c5bf23c58b73eb23a91d211e52f0b1f06c123
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-09 00:37:42 +0530

    Added new files

--- colortolieven.js
+++ colortolieven.js
@@ -1,7 +1,15 @@
 //add a highlit class to ever even  item in a lsit
- let even =document.querySelectorAll(" ul li:nth-child(2n");
+ let even =document.querySelectorAll(" ul li:nth-child(even)");
 
  even.forEach(function(element){
     element.classList.add("highlight");
 
  });
+
+ let p= document.querySelectorAll("p");
+  
+ p.forEach(function(elem){
+    elem.style.fontSize="20px";
+ })
+
+
